8849c0bcebf541eab02a53fe76dedeb1a84669ea
Pavel Siarzhantau <pavel.siarzhantau@gmail.com>
2022-12-08 19:09:56 +0100

    feat: add boatsNearMe component, refactor boatSearchResult component

--- force-app/main/default/lwc/boatSearchResults/boatSearchResults.js
+++ force-app/main/default/lwc/boatSearchResults/boatSearchResults.js
@@ -2,35 +2,52 @@ import { LightningElement, api, wire, track } from 'lwc';
 import { publish, MessageContext } from 'lightning/messageService';
 import BOATMC from '@salesforce/messageChannel/BoatMessageChannel__c';
 import getBoats from '@salesforce/apex/BoatDataService.getBoats';
+import updateBoatList from '@salesforce/apex/BoatDataService.updateBoatList';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { refreshApex } from '@salesforce/apex';
+
+import BOAT_NAME from '@salesforce/schema/Boat__c.Name';
+import BOAT_LENGTH from '@salesforce/schema/Boat__c.Length__c';
+import BOAT_PRICE from '@salesforce/schema/Boat__c.Price__c';
+import BOAT_DESCRIPTION from '@salesforce/schema/Boat__c.Description__c';
 
 const SUCCESS_TITLE = 'Success';
 const MESSAGE_SHIP_IT = 'Ship it!';
 const SUCCESS_VARIANT = 'success';
 const ERROR_TITLE = 'Error';
 const ERROR_VARIANT = 'error';
+const COLUMNS = [
+    { label: 'Name', fieldName:  BOAT_NAME.fieldApiName, editable: true },
+    { label: 'Length', fieldName: BOAT_LENGTH.fieldApiName, type: 'number', editable: true},
+    { label: 'Price', fieldName: BOAT_PRICE.fieldApiName, type: 'currency', editable: true},
+    { label: 'Description', fieldName: BOAT_DESCRIPTION.fieldApiName, editable: true},        
+    ];
 export default class BoatSearchResults extends LightningElement {
 
+  _title;
+  _message;
+  _variant;
+  _wiredData;
   selectedBoatId;
-  columns = [];
+  columns = COLUMNS;
   boatTypeId = '';
   @track boats;
   isLoading = false;
-    _wiredData;
+    
   // wired message context
   @wire(MessageContext)
   messageContext;
     
   // wired getBoats method
   @wire(getBoats,{boatTypeId : '$boatTypeId'})  
-  wiredBoats({ data, error }) {
-      this._wiredData = { data, error };
+  wiredBoats(response) {
+      const { data, error } = response;
+      this.boats = response;
       if (data) {
-        this.boats = data;
-        console.log(JSON.stringify(this.boats));
         this.isLoading = true;
         this.notifyLoading(this.isLoading);
       } else if (error) {
-          console.error(error);
+          console.log(error);
       }
   }
   
@@ -45,17 +62,28 @@ export default class BoatSearchResults extends LightningElement {
   
   // this public function must refresh the boats asynchronously
   // uses notifyLoading
-  refresh() { }
+    @api
+    async refresh() {
+        this.isLoading = false;
+        this.notifyLoading(this.isLoading);
+        await refreshApex(this.boats);
+        this.isLoading = true;
+        await this.notifyLoading(this.isLoading);
+        
+   }
   
   // this function must update selectedBoatId and call sendMessageService
     updateSelectedTile(event) {
         this.selectedBoatId = event.detail.boatId;
         console.log('boatSearchResutls: ', this.selectedBoatId);
+        this.sendMessageService(this.selectedBoatId);
    }
   
   // Publishes the selected boat Id on the BoatMC.
-  sendMessageService(boatId) { 
+    sendMessageService(boatId) { 
     // explicitly pass boatId to the parameter recordId
+        publish(this.messageContext, BOATMC, { recordId: boatId });
+    
   }
   
   // The handleSave method must save the changes in the Boat Editor
@@ -63,14 +91,37 @@ export default class BoatSearchResults extends LightningElement {
   // Apex method updateBoatList(Object data).
   // Show a toast message with the title
   // clear lightning-datatable draft values
-  handleSave(event) {
+  handleSave(event) {   
     // notify loading
-    const updatedFields = event.detail.draftValues;
+      this.isLoading = false;
+      this.notifyLoading(this.isLoading);
+      const updatedFields = event.detail.draftValues;
+      console.log('updatedFields: ' + updatedFields);
     // Update the records via Apex
     updateBoatList({data: updatedFields})
-    .then(() => {})
-    .catch(error => {})
-    .finally(() => {});
+        .then(() => {
+            this.isLoading = true;
+            this.notifyLoading(this.isLoading);
+            this._title = SUCCESS_TITLE;
+            this._message = MESSAGE_SHIP_IT;
+            this._variant = SUCCESS_VARIANT;
+            this.template.querySelector("lightning-datatable").draftValues = [];
+            this.refresh();
+            this.template.querySelector("c-boats-near-me").updateBoatsInfo();
+        })
+        .catch(error => {
+            this._title = ERROR_TITLE;
+            this._message = error.getMessage();
+            this._variant = ERROR_VARIANT;
+        })
+        .finally(() => {
+            const evt = new ShowToastEvent({
+                title: this._title,
+                message: this._message,
+                variant: this._variant,
+            });
+            this.dispatchEvent(evt);
+        });
   }
   // Check the current value of isLoading before dispatching the doneloading or loading custom event
     notifyLoading(isLoading) {
